4ec355e5c54e294ae8c8b6f4b4915f1ada1c4aac
Colin-K88 <colinkee@hotmail.com>
2022-02-16 13:33:56 +0000

    settings update

--- ecommercestore/settings.py
+++ ecommercestore/settings.py
@@ -176,6 +176,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ecommerce-store1'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -185,7 +191,7 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
+    STATICFILES_LOCATION = 'static/css'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
